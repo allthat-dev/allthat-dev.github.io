6f94d628509636e422798818760d1e7733cbf327
Dongsu Jang <77693+iolo@users.noreply.github.com>
2024-07-30 11:44:36 +0900

    modify theme switcher

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 (function (document, localStorage) {
   const root = document.documentElement;
-  const themeSelect = document.getElementById('theme-select');
+  const themeCheck = document.getElementById('themeCheck');
 
   function setTheme(theme) {
-    root.classList.remove('system-theme', 'light-theme', 'dark-theme');
     switch (theme) {
-      case 'system':
-        root.classList.add('system-theme');
-        break;
       case 'light':
+        root.classList.remove('dark-theme');
         root.classList.add('light-theme');
         root.style.setProperty('--bg-color', '#ffffff');
         root.style.setProperty('--text-color', '#333333');
         break;
       case 'dark':
+        root.classList.remove('light-theme');
         root.classList.add('dark-theme');
         root.style.setProperty('--bg-color', '#333333');
         root.style.setProperty('--text-color', '#ffffff');
         break;
+        // use system theme
+        break;
     }
     localStorage.setItem('theme', theme);
   }
+  themeCheck.addEventListener('change', (e) => setTheme(e.target.checked ? 'dark' : 'light'));
 
-  themeSelect.addEventListener('change', (e) => setTheme(e.target.value));
-
-  const savedTheme = localStorage.getItem('theme') || 'system';
-  themeSelect.value = savedTheme;
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+  const savedTheme = localStorage.getItem('theme') || systemTheme;
+  themeCheck.checked = savedTheme;
   setTheme(savedTheme);
 
   const logos = ['(*_*)', '(o_o)', '(-_-)', '(`_`)', '(>_<)'];
@@ -34,5 +34,5 @@
   setInterval(() => {
     logoEl.innerHTML = logos[logoIndex];
     logoIndex = ++logoIndex % logos.length;
-  }, 3000);
+  }, 1000);
 })(document, localStorage);
